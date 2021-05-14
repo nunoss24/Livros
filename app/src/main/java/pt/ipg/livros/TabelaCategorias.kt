af4f00b710eb3db6fcf5a88e25fd94dcd3bf944c
Noel Lopes <noel@ipg.pt>
2021-05-14 11:46:46 +0100

    Criadas funções CRUD para as tabelas da base de dados

--- app/src/main/java/pt/ipg/livros/TabelaCategorias.kt
+++ app/src/main/java/pt/ipg/livros/TabelaCategorias.kt
@@ -1,5 +1,7 @@
 package pt.ipg.livros
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
@@ -11,6 +13,29 @@ class TabelaCategorias(db: SQLiteDatabase) {
                 BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 CAMPO_NOME + " TEXT NOT NULL" +
                 ")")
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
     }
 
     companion object {
--- app/src/main/java/pt/ipg/livros/TabelaLivros.kt
+++ app/src/main/java/pt/ipg/livros/TabelaLivros.kt
@@ -1,5 +1,7 @@
 package pt.ipg.livros
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
@@ -16,6 +18,29 @@ class TabelaLivros(db: SQLiteDatabase) {
                 "FOREIGN KEY(" + CAMPO_ID_CATEGORIA + ") " +
                     "REFERENCES " + TabelaCategorias.NOME_TABELA  +
                 ")")
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(TabelaCategorias.NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(TabelaCategorias.NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(TabelaCategorias.NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(TabelaCategorias.NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
     }
 
     companion object {
